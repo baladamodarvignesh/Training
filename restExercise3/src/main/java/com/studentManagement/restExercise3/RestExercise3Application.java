a1e145b34a0e4e9f4c0b2eb5cb054bd5d5f97e36
balavignesh <balavignesh.premkumar@gmail.com>
2019-05-31 12:54:59 +0530

    New Configuration pom changes

--- restExercise3/src/main/java/com/studentManagement/restExercise3/RestExercise3Application.java
+++ restExercise3/src/main/java/com/studentManagement/restExercise3/RestExercise3Application.java
@@ -1,11 +1,15 @@
 package com.studentManagement.restExercise3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestExercise3Application {
 
+//	@Value("${server.port}")
+//	private String serverPort;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestExercise3Application.class, args);
 	}
